b6016367754697ccf7aceb15a2cba26ef0295998
m <m@MacBook-Pro-m.local>
2026-04-20 20:31:55 +0300

    feat: frontend map and cors

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # В идеале здесь должен быть твой IP, но для теста ставим "*"
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
     return {"message": "Antisnow Backend is Running!"}
