45a766b3db9d9998699c27fea423ed9359c2b51f
m <m@MacBook-Pro-m.local>
2026-04-20 20:41:20 +0300

    fix: add CORSMiddleware import

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware  # ВОТ ЭТОЙ СТРОКИ НЕ ХВАТАЛО
+from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+import os
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # В идеале здесь должен быть твой IP, но для теста ставим "*"
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
